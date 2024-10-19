ef7e259e2a9017da1d3e816374df3870baad9e95
tsousa111 <tiagao2001@hotmail.com>
2024-10-19 16:37:36 +0100

    fix setpassword

--- app/page.tsx
+++ app/page.tsx
@@ -63,7 +63,6 @@ function LoginForm() {
           <label
             htmlFor="password"
             className="block text-sm font-medium leading-6 text-gray-900"
-            onChange={(e) => setPassword(e.target.value)}
           >
             Password
           </label>
@@ -84,6 +83,7 @@ function LoginForm() {
             autoComplete="current-password"
             required
             className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
       </div>
