275abb8c9c9eda88ba74aa4cc776cef889ce61d1
tsousa111 <tiagao2001@hotmail.com>
2024-11-10 00:37:33 +0000

    landing page redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+import { redirect } from "next/navigation";
 export default function Home() {
-  return;
+  // for now this page is just a redirect to the gallery
+  // eventually it will be a landing page
+  return redirect("/gallery");
 }
